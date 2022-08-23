7231a2eb58b031abda533cf0e378a6388a0ccc82
Jeyhun Rahimli <rahimlijeyhun77@gmail.coml>
2022-08-23 20:50:53 +0200

    add: inserting user into db

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,5 +1,42 @@
-const postRegister = async(req, res, next) => {
-    res.json({ ststus: true, statusCode: res.statusCode, message: "Register API Endpoint" })
-}
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
+const postRegister = async (req, res, next) => {
+  try {
+    const { email, password, repeat_password, fullname } = req.body;
+
+    // check if user exists
+    const userExists = await User.exists({ email });
+    if (userExists) {
+      return res.status(409).send("Email alrady in use.");
+    }
+    if (password != repeat_password) {
+      return res.status(409).send("Password doesn't match.");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    // create a user object
+    const user = User.create({
+      fullname,
+      email,
+      password: encryptedPassword,
+      repeat_password: encryptedPassword,
+    });
+
+    // create JWT
+    const token = "JW TOKEN";
+
+    res.status(201).json({
+      userDetails: {
+        fullname: user.fullname,
+        email: user.email,
+        token,
+        password: user.password
+      }
+    });
+  } catch (e) {
+    return res.status(500).send('Error occured. Please try again.')
+  }
+};
 
 module.exports = postRegister;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,6 +14,7 @@ const loginSchema = Joi.object({
 const registerSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).max(12).required(),
+    repeat_password: Joi.string().min(6).max(12).required(),
     fullname: Joi.string().min(3).required(),
 })
 
