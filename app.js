8a6c256b520b37ecd0100abd9c2caf39b4781934
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-03-03 08:05:07 +0100

    add: postRegister generate random number added

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 // app router
 app.use("/", indexRouter);
--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -16,35 +16,42 @@ const postRegister = async (req, res, next) => {
       return res.status(409).json({ statusMessage: "Password doesn't match." });
     }
 
+    // create password hash
     const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // TODO: generate random number
+    const randomNum = Math.floor(Math.random() * 900000) + 100000;
+    // TODO: send email
+
     // create a user object
     const user = User.create({
       fullname,
       email,
       password: encryptedPassword,
       repeat_password: encryptedPassword,
+      confirmationNumber: randomNum
     });
 
     // generate JWT
-    const token = jwt.sign(
-      {
-        userId: (await user)._id,
-        email: (await user).email,
-      },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "604800",
-      }
-    );
+    // const token = jwt.sign(
+    //   {
+    //     userId: (await user)._id,
+    //     email: (await user).email,
+    //   },
+    //   process.env.TOKEN_KEY,
+    //   {
+    //     expiresIn: "604800",
+    //   }
+    // );
 
     res.status(201).json({
       userDetails: {
         fullname: (await user).fullname,
         email: (await user).email,
-        token,
       },
     });
   } catch (e) {
+    console.log(e)
     return res
       .status(500)
       .json({ statusMessage: "Error occured. Please try again." });
